b1a3fd02b3ac59107b0b3afcaa21bbbb7c2182a2
abbasaziz <abbas9111@gmail.com>
2026-03-11 01:33:59 +0500

    Jaeger updates for main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
+    
 # 1. Setup the Tracer
 resource = Resource(attributes={
     SERVICE_NAME: "fastapi-service"
